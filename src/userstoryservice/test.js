896b6da76e6728e45dff9a3a2f8555fd03eb253a
Subham Kumar (Student) <skuma251@sundevils.asu.edu>
2023-03-28 22:48:06 -0700

    Added test case for creating user story

---
+++ src/userstoryservice/test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+const app = require("./index");
+
+describe("User story Microservice", () => {
+  describe("POST /createUserstory", () => {
+    it("should return a 201 response with the user story id", async () => {
+      const response = await request(app)
+        .post("/createUserstory")
+        .set("Accept", "application/json")
+        .set(
+          "Authorization",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMTUzMDU3LCJqdGkiOiIxNDY3MzFlMGEzMzg0MjMzYmI0MDdiYWM3YTczNzYxOSIsInVzZXJfaWQiOjU1OTIxNX0.RzYvAwZTNYI-tm2J4Fz0FOOBxIcXvzEWolA38a5p-jQ"
+        )
+        .send({ project: 711904, subject: "test user story" });
+      expect(response.status).toBe(201);
+      expect(response.body.userstoryId).toBeDefined();
+    });
+    it("should return a 500 response with an error message if user story is not created", async () => {
+      const response = await request(app)
+        .post("/createUserstory")
+        .set("Accept", "application/json")
+        .set(
+          "Authorization",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMTUzMDU3LCJqdGkiOiIxNDY3MzFlMGEzMzg0MjMzYmI0MDdiYWM3YTczNzYxOSIsInVzZXJfaWQiOjU1OTIxNX0.RzYvAwZTNYI-tm2J4Fz0FOOBxIcXvzEWolA38a5p-jQ"
+        )
+        .send({ project: 71190, subject: "test user story" });
+      expect(response.status).toBe(500);
+    });
+  });
+});
