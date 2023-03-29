2edbf0c79153784fc7ecc62a5e6bcd7fd36dbc9d
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2023-03-29 15:50:56 -0700

    Adding Update User Story functionality

--- src/userstoryservice/index.js
+++ src/userstoryservice/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createUserstory } = require("./logic");
+const {updateUserstory} = require("./logic")
 
 const app = express();
 const port = 3003;
@@ -11,6 +12,39 @@ app.post("/createUserstory", async (req, res) => {
   const { project, subject } = req.body;
   const token = req.headers.authorization.split(" ")[1];
   const userstoryData = await createUserstory(project, subject, token);
+  if (!userstoryData.success) {
+    return res.status(500).send({
+      userstoryData,
+    });
+  }
+  return res.status(201).send(userstoryData);
+});
+
+// Endpoint for updating a user story
+app.patch("/updateUserstory", async (req, res) => {
+  const userstoryId  = req.body.id;
+  const version = req.body.version;
+  var parameters = {};
+  if(req.body.description !== undefined) {
+    parameters.description = req.body.description;
+  }
+  if(req.body.assigned_to !== undefined) {
+    parameters.assigned_to = req.body.assigned_to;
+  }
+  if(req.body.is_closed !== undefined) {
+    parameters.is_closed = req.body.is_closed;
+  }
+  if(req.body.tags !== undefined) {
+    parameters.tags = req.body.tags;
+    }
+  if(req.body.points !== undefined) {
+    parameters.points = req.body.points;
+  }
+  parameters.version = version;
+
+
+  const token = req.headers.authorization.split(" ")[1];
+  const userstoryData = await updateUserstory(userstoryId, parameters, token);
   if (!userstoryData.success) {
     return res.status(500).send({
       userstoryData,
--- src/userstoryservice/logic.js
+++ src/userstoryservice/logic.js
@@ -32,6 +32,38 @@ async function createUserstory(project, subject, token) {
   }
 }
 
+// Function to update user story
+async function updateUserstory(userstoryId, parameters, token) {
+  try {
+    const USERSTORY_UPDATE_API_URL = USERSTORY_API_URL + "/" + userstoryId
+    const response = await axios.patch(
+      USERSTORY_UPDATE_API_URL,
+        parameters
+      ,
+      { headers: { Authorization: `Bearer ${token}`} }
+    );
+    console.log("_______________");
+    console.log(response);
+    if (response.data.id) {
+      return {
+        success: true,
+        message: `User Story with ${userstoryId} successfully updates`,
+        userstoryId: response.data.id,
+      };
+    } else {
+      return {
+        success: false,
+        message: "Something went wrong while creating userstory",
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: "Error creating userstory" };
+  }
+}
+
+
 module.exports = {
   createUserstory,
+  updateUserstory
 };
