7fa5658645b7fd770b3e78d1ee0f6655dac123c0
Chenxi6132 <chenxix2@asu.edu>
2023-03-31 13:41:01 -0700

    implement add role function

--- src/AddRolesUS21/index.js
+++ src/AddRolesUS21/index.js
@@ -1,1 +1,53 @@
-This will have your endpoints of your microservices.
+//This will have your endpoints of your microservices.
+const express = require("express");
+const {createRoles,updateRole,getRoleDetails} = require("./logic");
+
+
+const app = express();
+const port = 3004;
+
+app.use(express.json());
+
+// Endpoint for creating a role
+app.post("/createRoles", async (req, res) => {
+  const { name, token } = req.body;
+  const result = await createRoles(name, token);
+  if (result.success) {
+    res.status(202).json(result);
+  } else {
+    res.status(500).json(result);
+  }
+});
+
+// Endpoint for updating a role
+app.patch("/updateRole", async (req, res) => {
+  const { roleId, parameters, token } = req.body;
+  const result = await updateRole(roleId, parameters, token);
+  if (result.success) {
+    res.status(202).json(result);
+  } else {
+    res.status(500).json(result);
+  }
+});
+
+// Endpoint for getting role details
+app.get("/getRoleDetails", async (req, res) => {
+  const { token, slugName, roleName } = req.query;
+  const result = await getRoleDetails(token, slugName, roleName);
+  if (result.success) {
+    res.status(202).json(result);
+  } else {
+    res.status(500).json(result);
+  }
+});
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(
+      `Add roles microservice running at http://localhost:${port}`
+    );
+  });
+  
+  module.exports = app;
--- src/AddRolesUS21/logic.js
+++ src/AddRolesUS21/logic.js
@@ -1,2 +1,101 @@
 //This will have the logic of the endpoints.
 
+const axios = require("axios");
+require("dotenv").config({ path: "../.env" });
+
+const ROLE_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles`;
+
+//create new roles
+async function createRoles(name, token)
+{
+    try {   
+        const response = await axios.post(
+            ROLE_API_URL,
+          {
+            name,
+          },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        if (response.data.id) {
+          return {
+            success: true,
+            message: `${name} role successfully created.`,
+            userstoryId: response.data.id,
+          };
+        } else {
+          return {
+            success: false,
+            message: "Something went wrong while creating role",
+          };
+        }
+        }catch (error) {
+        console.error(error);
+        return { success: false, message: "Error creating role" };
+    }
+}
+
+
+
+//update roles
+async function updateRole(roleId, parameters, token) {
+    try {
+        const ROLE_UPDATE_API_URL = ROLE_API_URL + "/" + roleId;
+        const response = await axios.patch(
+        ROLE_UPDATE_API_URL,
+        parameters,
+        { headers: { Authorization: `Bearer ${token}`} }
+        );
+        if (response.data.id) {
+        return {
+            success: true,
+            message: `Role with ${roleId} successfully updated`,
+            roleId: response.data.id,
+        };
+        } else {
+        return {
+            success: false,
+            message: "Something went wrong while updating role",
+        };
+        }
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: "Error updating role" };
+    }
+}
+
+//get roles details
+async function getRoleDetails(token, slugName, roleName){
+
+    try {
+      const ROLE_DETAILS_API_URL = ROLE_API_URL + "?project__slug=" + slugName
+      const response = await axios.get(
+        ROLE_DETAILS_API_URL,
+        { headers: { Authorization: `Bearer ${token}`} }
+      );
+        
+      const role = response.data.find(r => r.name === roleName);
+    
+      if (role) {
+        return {
+          success: true,
+          message: `${roleName} role successfully fetched.`,
+          role,
+        };
+      } else {
+        return {
+          success: false,
+          message: `${roleName} role not found in the ${projectSlug} project.`,
+        };
+      }
+    } catch (error) {
+      console.error(error);
+      return { success: false, message: "Error fetching role details" };
+    }
+  
+}
+    
+module.exports = {
+    createRoles,
+    updateRole,
+    getRoleDetails,
+  };
