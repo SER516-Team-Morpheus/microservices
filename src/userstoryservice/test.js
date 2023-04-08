97262aa136ce9aa672f71fed3db6a75ff8c22599
skuma251 <skuma251@asu.edu>
2023-04-08 11:15:07 -0700

    Created a new endpoint to fetch all user story for a project

--- src/userstoryservice/test.js
+++ src/userstoryservice/test.js
@@ -1,79 +1,107 @@
-const request = require("supertest");
-const app = require("./index");
+const request = require('supertest')
+const app = require('./index')
 
-describe("User story Microservices", () => {
-  describe("POST /createUserstory", () => {
-    it("should return a 201 response with the user story id", async () => {
+describe('User story Microservices', () => {
+  describe('POST /createUserstory', () => {
+    it('should return a 201 response with the user story id', async () => {
       const response = await request(app)
-        .post("/createUserstory")
-        .set("Accept", "application/json")
-        .set(
-          "Authorization",
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMzE5NTczLCJqdGkiOiJkN2ExNGQ1ZjkzY2I0MWM1YThjNDdkZmVhYmM2NGJlYiIsInVzZXJfaWQiOjU1OTIxNX0.456FyzZ7tnTOZ3g3WxLjm0TboVxyoQckZZaBfodLHi4"
-        )
-        .send({ project: 715445, subject: "test user story" });
-      expect(response.status).toBe(201);
-      expect(response.body.userstoryId).toBeDefined();
-    });
-    it("should return a 500 response with an error message if user story is not created", async () => {
-      const response = await request(app)
-        .post("/createUserstory")
-        .set("Accept", "application/json")
-        .set(
-          "Authorization",
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMzE5NTczLCJqdGkiOiJkN2ExNGQ1ZjkzY2I0MWM1YThjNDdkZmVhYmM2NGJlYiIsInVzZXJfaWQiOjU1OTIxNX0.456FyzZ7tnTOZ3g3WxLjm0TboVxyoQckZZaBfodLHi4"
-        )
-        .send({ project: 7119, subject: "test user story" });
-      expect(response.status).toBe(500);
-    });
-  });
-
-
-  describe("GET /getAllUserStoryDetails", () => {
-    it("should return a 201 response with the user story id", async () => {
-      const response = await request(app)
-        .get("/getAllUserStoryDetails")
-        .set("Accept", "application/json")
+        .post('/createUserstory')
+        .set('Accept', 'application/json')
         .send({
-          username: "SERtestuser",
-          password: "testuser",
-          projectname:"testProject",
-          userstoryname:"test US3"
-      });
-      expect(response.status).toBe(201);
-      expect(response.body.parameters).toBeDefined();
-    });
-    it("should return a 500 response with the user story id", async () => {
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'testProject',
+          subject: 'test US'
+        })
+      expect(response.status).toBe(201)
+      expect(response.body.userstoryId).toBeDefined()
+    })
+    it('should return a 500 response with an error message if user story is not created', async () => {
       const response = await request(app)
-        .get("/getAllUserStoryDetails")
-        .set("Accept", "application/json")
+        .post('/createUserstory')
+        .set('Accept', 'application/json')
         .send({
-          username: "SERtestuser",
-          password: "testuser",
-          projectname:"testProject",
-          userstoryname:"test USTest"
-      });
-      expect(response.status).toBe(500);
-    });
-  });
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'noProject',
+          subject: 'test US4'
+        })
+      expect(response.status).toBe(500)
+    })
+  })
 
-  describe("PATCH /updateUserStory", () => {
-    it("should return a 201 response with an error message if user story is not created", async () => {
+  describe('GET /getUserStory', () => {
+    it('should return a 200 response with the user story id and name', async () => {
       const response = await request(app)
-        .patch("/updateUserstory")
-        .set("Accept", "application/json")
-        .send({ username: "SERtestuser",
-                password: "testuser",
-                projectname:"testProject",
-                userstoryname:"test US3",
-                tags:["New-Feature"],
-                points: {
-                    "UX": "1",
-                    "Back": "8",
-                    "Front":"5"
-            } 
-          });
-      expect(response.status).toBe(201);
-    });
-  });
-});
+        .get('/getUserStory')
+        .set('Accept', 'application/json')
+        .query({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'testProject'
+        })
+      expect(response.status).toBe(200)
+      expect(response.body.userStory).toBeDefined()
+    }, 20000)
+    it('should return a 500 response with an error message if user story is not created', async () => {
+      const response = await request(app)
+        .get('/getUserStory')
+        .set('Accept', 'application/json')
+        .query({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'noProject'
+        })
+      expect(response.status).toBe(500)
+    })
+  })
+
+  describe('GET /getUserStoryDetails', () => {
+    it('should return a 200 response with the user story id', async () => {
+      const response = await request(app)
+        .get('/getUserStoryDetails')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US3'
+        })
+      expect(response.status).toBe(200)
+      expect(response.body.parameters).toBeDefined()
+    })
+    it('should return a 500 response with the user story id', async () => {
+      const response = await request(app)
+        .get('/getUserStoryDetails')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test USTest'
+        })
+      expect(response.status).toBe(500)
+    })
+  })
+
+  describe('PATCH /updateUserStory', () => {
+    it('should return a 201 response with an error message if user story is not created', async () => {
+      const response = await request(app)
+        .patch('/updateUserstory')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US3',
+          tags: ['feature'],
+          points: {
+            UX: '1',
+            Back: '8',
+            Front: '5'
+          }
+        })
+      expect(response.status).toBe(201)
+    })
+  })
+})
