2c3791182f80aecc0224a33e3b4de1ab4f84b707
sambhavv14asu <skapoo26@asu.edu>
2023-04-08 01:57:49 -0700

    Writing the functionality to edit role of the member

--- src/memberservice/logic.js
+++ src/memberservice/logic.js
@@ -1,102 +1,133 @@
-const axios = require("axios");
-require("dotenv").config({ path: "../.env" });
+const axios = require('axios')
+require('dotenv').config({ path: '../.env' })
 
-const MEMBER_API_URL = `${process.env.TAIGA_API_BASE_URL}/memberships`;
+const MEMBER_API_URL = `${process.env.TAIGA_API_BASE_URL}/memberships`
 
-const AUTH_URL = `${process.env.AUTHENTICATE_URL}`;
+const AUTH_URL = `${process.env.AUTHENTICATE_URL}`
+const AUTH_API_URL = `${process.env.TAIGA_API_BASE_URL}/auth`
 
-//Function to get auth token from authenticate api
-async function getToken(username, password) {
+// Function to get auth token from authenticate api
+async function getToken (username, password) {
   try {
     const response = await axios.post(AUTH_URL, {
-      type: "normal",
+      type: 'normal',
       username,
-      password,
-    });
+      password
+    })
     if (response.data.token) {
-      return response.data.token;
+      return response.data.token
     } else {
-      return { auth_token: "NULL" };
+      return { auth_token: 'NULL' }
     }
   } catch (error) {
-    return { auth_token: "NULL" };
+    return { auth_token: 'NULL' }
   }
 }
 
 // Function to get the roles
-async function getRoleId(token, projectId) {
-    const ROLES_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles?project=${projectId}`;
-    try {
-      const response = await axios.get(ROLES_API_URL, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      const firstObject = response.data[0];
-      return firstObject.id;
-    } catch (error) {
-      return { success: false, message: "Error getting roles" };
-    }
+async function getRoleId (token, projectId) {
+  const ROLES_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles?project=${projectId}`
+  try {
+    const response = await axios.get(ROLES_API_URL, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    const firstObject = response.data[0]
+    return firstObject.id
+  } catch (error) {
+    return { success: false, message: 'Error getting roles' }
   }
+}
 
 // Function to create new member
-async function createMember(username, project, role, token) {
-    try {
-      const response = await axios.post(
-        MEMBER_API_URL,
-        {
-          project,
-          role,
-          username,
-        },
-        { headers: { Authorization: `Bearer ${token}` }  }
-      );
-      if (response.data.id) {
-        return { success: true, memberId: response.data.id };
-      } else {
-        return {
-          success: false,
-          message: "Something went wrong while creating a member",
-        };
+async function createMember (username, project, role, token) {
+  try {
+    const response = await axios.post(
+      MEMBER_API_URL,
+      {
+        project,
+        role,
+        username
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+    if (response.data.id) {
+      return { success: true, memberId: response.data.id }
+    } else {
+      return {
+        success: false,
+        message: 'Something went wrong while creating a member'
       }
-    } catch (error) {
-      return { success: false, message: "Error creating member" };
     }
+  } catch (error) {
+    return { success: false, message: 'Error creating member' }
   }
+}
 
-async function getMembers(token, projectId){
-  const MEMBERS_API_URL = `${MEMBER_API_URL}?project=${projectId}`;
+async function getMembers (token, projectId) {
+  const MEMBERS_API_URL = `${MEMBER_API_URL}?project=${projectId}`
   try {
     const response = await axios.get(MEMBERS_API_URL, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    
-    return { success: true, data: response.data };
+      headers: { Authorization: `Bearer ${token}` }
+    })
+
+    return { success: true, data: response.data }
   } catch (error) {
-    return { success: false, message: "Error getting roles" };
+    return { success: false, message: 'Error getting roles' }
   }
 }
 // Function to delete a member
-async function deleteMember(memberId, projectId, token) {
-  const MEMBER_API_URL = `${process.env.TAIGA_API_BASE_URL}/memberships/${memberId}?project=${projectId}`;
+async function deleteMember (memberId, projectId, token) {
+  const MEMBER_API_URL = `${process.env.TAIGA_API_BASE_URL}/memberships/${memberId}`
   try {
     const response = await axios.delete(
       MEMBER_API_URL,
       {
         headers: {
-          Authorization: `Bearer ${token}`,
-        },
+          Authorization: `Bearer ${token}`
+        }
       }
-    );
+    )
 
-    return { success: true, data: response.data };
+    return { success: true, data: response.data }
   } catch (error) {
-    return { success: false, message: error.message };
+    return { success: false, message: error.message }
   }
 }
-  
-  module.exports = {
-    getRoleId,
-    createMember,
-    getToken,
-    getMembers,
-    deleteMember
-  };
+
+// Function to update a member role
+async function updateMember (username, password, roleId, memberId) {
+  const response1 = await axios.post(AUTH_API_URL, {
+    type: 'normal',
+    username,
+    password
+  })
+  const authToken = response1.data.auth_token
+  const MEMBER_API_URL = `${process.env.TAIGA_API_BASE_URL}/memberships/${memberId}`
+  try {
+    const response = await axios.patch(
+      MEMBER_API_URL,
+      {
+        role: roleId
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${authToken}`
+        }
+      }
+    )
+
+    return { success: true, data: response.data }
+  } catch (error) {
+    return { success: false, message: 'Error editing member role' }
+  }
+}
+
+module.exports = {
+  getRoleId,
+  createMember,
+  getToken,
+  getMembers,
+  deleteMember,
+  updateMember
+}
--- src/memberservice/test.js
+++ src/memberservice/test.js
@@ -1,62 +1,62 @@
-const request = require("supertest");
-const app = require("./index");
-const axios = require("axios");
+const request = require('supertest')
+const app = require('./index')
+const axios = require('axios')
 
-describe("Member Microservice", () => {
-  describe("POST /createMember", () => {
-    it("should return a 201 response", async () => {
+describe('Member Microservice', () => {
+  describe('POST /createMember', () => {
+    it('should return a 201 response', async () => {
       const response = await request(app)
-        .post("/createMember")
-        .set("Accept", "application/json")
+        .post('/createMember')
+        .set('Accept', 'application/json')
         .send({
-          username: "sambhavv14asu",
-          password: "Wisdommarrt_01",
-          member: "uniqueASU",
-          projectId: 720797,
-        });
-      expect(response.status).toBe(201);
-      expect(response.body.success).toBe(true);
-      expect(response.body.memberId).toBeDefined();
-    });
-    it("should return a 500 response", async () => {
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          member: 'uniqueASU',
+          projectId: 720797
+        })
+      expect(response.status).toBe(201)
+      expect(response.body.success).toBe(true)
+      expect(response.body.memberId).toBeDefined()
+    })
+    it('should return a 500 response', async () => {
       const response = await request(app)
-        .post("/createMember")
-        .set("Accept", "application/json")
+        .post('/createMember')
+        .set('Accept', 'application/json')
         .send({
-          username: "sambhavv14asu",
-          password: "Wisdommarrt_01",
-          member: "uniqueASU",
-          projectId: 9,
-        });
-      expect(response.status).toBe(500);
-      expect(response.body.success).toBe(false);
-      expect(response.body.message).toBeDefined();
-    });
-  });
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          member: 'uniqueASU',
+          projectId: 9
+        })
+      expect(response.status).toBe(500)
+      expect(response.body.success).toBe(false)
+      expect(response.body.message).toBeDefined()
+    })
+  })
 
-  describe("GET /getMembers", () => {
-    it("should return a 201 response ", async () => {
+  describe('GET /getMembers', () => {
+    it('should return a 201 response ', async () => {
       const response = await request(app)
-        .get("/getMembers")
-        .set("Accept", "application/json")
+        .get('/getMembers')
+        .set('Accept', 'application/json')
         .send({
-          username:"sambhavv14asu",
-          password:"Wisdommarrt_01",
-          projectId: 720797,
-      });
-      expect(response.status).toBe(201);
-      expect(response.body.body).toBeDefined();
-    });
-    it("should return a 500 response ", async () => {
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          projectId: 720797
+        })
+      expect(response.status).toBe(201)
+      expect(response.body.body).toBeDefined()
+    })
+    it('should return a 500 response ', async () => {
       const response = await request(app)
-        .get("/getMembers")
-        .set("Accept", "application/json")
+        .get('/getMembers')
+        .set('Accept', 'application/json')
         .send({
-          username:"sambhavv14asu",
-          password:"Wisdommarrt_01",
-          projectId:720
-      });
-      expect(response.status).toBe(500);
-    });
-  });
-});
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          projectId: 720
+        })
+      expect(response.status).toBe(500)
+    })
+  })
+})
