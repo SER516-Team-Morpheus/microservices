ab36bc45d215d7a46841d619f3bc391a7e4e9836
Amarjeet Singh <amarjeetsingh@Amarjeets-MacBook-Air.local>
2023-04-07 18:52:07 -0700

    Adding test case for update task and some changes

--- src/taskservice/logic.js
+++ src/taskservice/logic.js
@@ -1,145 +1,126 @@
-const axios = require("axios");
-require("dotenv").config({ path: "../.env" });
+const axios = require('axios');
+require('dotenv').config({ path: '../.env' });
 
 const TASK_API_URL = `${process.env.TAIGA_API_BASE_URL}/tasks`;
 const TOKEN_API_URL = `${process.env.TAIGA_API_BASE_URL}/auth`;
-const USERSTORY_API_URL = `${process.env.TAIGA_API_BASE_URL}/userstories`
-const TASK_UPDATE_API_URL=`${process.env.TAIGA_API_BASE_URL}/task-statuses`;
+const USERSTORY_API_URL = `${process.env.TAIGA_API_BASE_URL}/userstories`;
 
-
-//Function to get auth token from authenticate api
+// Function to get auth token from authenticate api
 async function getToken(username, password) {
   try {
     const response = await axios.post(TOKEN_API_URL, {
-      type: "normal",
+      type: 'normal',
       username,
       password,
     });
     if (response.data.auth_token) {
       return response.data.auth_token;
-    } else {
-      return { auth_token: "NULL" };
     }
+    return { auth_token: 'NULL' };
   } catch (error) {
-    return { auth_token: "NULL" };
+    return { auth_token: 'NULL' };
   }
 }
 
-async function getUserStoryDetails(token, slugName,userstoryName){
+async function getUserStoryDetails(token, slugName, userstoryName) {
   try {
-    const USERSTORY_DETAILS_API_URL = USERSTORY_API_URL + "?project__slug=" + slugName
+    const USERSTORY_DETAILS_API_URL = `${USERSTORY_API_URL}?project__slug=${slugName}`;
     const response = await axios.get(
       USERSTORY_DETAILS_API_URL,
-      { headers: { Authorization: `Bearer ${token}`} }
+      { headers: { Authorization: `Bearer ${token}` } },
     );
-      
-      var parameters = {};
-      for (let i = 0; i < response.data.length; i++) {
-        if(response.data[i].subject === userstoryName)
-        {
-          parameters.id = response.data[i].id;
-          parameters.version = response.data[i].version;
-          parameters.ref = response.data[i].ref;
-          parameters.projectid = response.data[i].project;
 
-        }
-
+    const parameters = {};
+    for (let i = 0; i < response.data.length; i += 1) {
+      if (response.data[i].subject === userstoryName) {
+        parameters.id = response.data[i].id;
+        parameters.version = response.data[i].version;
+        parameters.ref = response.data[i].ref;
+        parameters.projectid = response.data[i].project;
       }
-      if(parameters.id)
-      {
-        return {
-          success: true,
-          message: `successfully fetched details`,
-          parameters
-        };
-      }
-      else
-      {
-        return {
-          success: false,
-          message: "User Story not found",
-        };
-        }
-  }
-  catch(error)
-  {
-    return { success: false, message: "Error fetching userstories details" };
-  }
+    }
+    if (parameters.id) {
+      return {
+        success: true,
+        message: 'successfully fetched details',
+        parameters,
+      };
+    }
 
+    return {
+      success: false,
+      message: 'User Story not found',
+    };
+  } catch (error) {
+    return { success: false, message: 'Error fetching userstories details' };
+  }
 }
 
+// eslint-disable-next-line camelcase
 async function createTask(project, user_story, subject, token) {
   try {
     const response = await axios.post(
       TASK_API_URL,
       {
         project,
+        // eslint-disable-next-line camelcase
         user_story,
-        subject
+        subject,
       },
-      { headers: { Authorization: `Bearer ${token}` } }
+      { headers: { Authorization: `Bearer ${token}` } },
     );
     if (response.data.id) {
       return {
         success: true,
-        message: `Tak ${subject} successfully created.`,
+        message: `Task ${subject} successfully created.`,
         taskId: response.data.id,
       };
-    } else {
+    }
+    return {
+      success: false,
+      message: 'Something went wrong while creating task',
+    };
+  } catch (error) {
+    return { success: false, message: 'Error creating task' };
+  }
+}
+
+async function getTaskDetails(token, slugName, taskname) {
+  try {
+    const TASK_DETAILS_API_URL = `${TASK_API_URL}?project__slug=${slugName}`;
+    const response = await axios.get(
+      TASK_DETAILS_API_URL,
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+    const parameters = {};
+    for (let i = 0; i < response.data.length; i += 1) {
+      if (response.data[i].subject === taskname) {
+        parameters.id = response.data[i].id;
+        parameters.user_story = response.data[i].user_story;
+        parameters.version = response.data[i].version;
+        parameters.status_id = response.data[i].status;
+      }
+    }
+    if (parameters.id) {
       return {
-        success: false,
-        message: "Something went wrong while creating task",
+        success: true,
+        message: 'successfully fetched details',
+        parameters,
       };
     }
+
+    return {
+      success: false,
+      message: 'Task not found',
+    };
   } catch (error) {
-    return { success: false, message: "Error creating task" };
+    return { success: false, message: 'Error fetching task' };
   }
 }
 
-async function getTaskDetails(token, slugName, taskname){
-  try{
-    const TASK_DETAILS_API_URL = TASK_API_URL + "?project__slug=" + slugName
-    const response = await axios.get(
-                    TASK_DETAILS_API_URL,
-                      { headers: { Authorization: `Bearer ${token}`} }
-                    );
-    var parameters = {};
-    for (let i = 0; i < response.data.length; i++) {
-      if(response.data[i].subject === taskname)
-        {
-            parameters.id = response.data[i].id;
-            parameters.user_story = response.data[i].user_story;
-            parameters.version = response.data[i].version;
-            parameters.status_id = response.data[i].status;
-
-        }
-      
-      }
-        if(parameters.id)
-        {
-          return {
-            success: true,
-            message: `successfully fetched details`,
-            parameters
-          };
-        }
-        else
-        {
-          return {
-            success: false,
-            message: "Task not found",
-          };
-          }
-
-  }catch (error) {
-    return { success: false, message: "Error fetching task" };
-  }
-
-}
-
-async function updateTaskDetails(token, taskId,parameters){
-  try{
-    const TASK_UPDATE_API_URL = TASK_API_URL + "/" + taskId;
+async function updateTaskDetails(token, taskId, parameters) {
+  try {
+    const TASK_UPDATE_API_URL = `${TASK_API_URL}/${taskId}`;
     const response = await axios.patch(TASK_UPDATE_API_URL, parameters, {
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -149,16 +130,13 @@ async function updateTaskDetails(token, taskId,parameters){
         message: `Task with id ${taskId} successfully updated`,
         taskId: response.data.id,
       };
-    } else {
-      return {
-        success: false,
-        message: "Something went wrong while updating task",
-      };
     }
-
-  }
-  catch (error) {
-    return { success: false, message: "Error updating the task" };
+    return {
+      success: false,
+      message: 'Something went wrong while updating task',
+    };
+  } catch (error) {
+    return { success: false, message: 'Error updating the task' };
   }
 }
 
@@ -167,5 +145,5 @@ module.exports = {
   getToken,
   getUserStoryDetails,
   getTaskDetails,
-  updateTaskDetails
+  updateTaskDetails,
 };
--- src/taskservice/test.js
+++ src/taskservice/test.js
@@ -1,31 +1,67 @@
-const request = require("supertest");
-const app = require("./index");
+const request = require('supertest');
+const app = require('./index');
 
-describe("Task Microservices", () => {
-  describe("POST /createTask", () => {
-   it("should return a 201 response with the taskid and message", async () => {
+describe('Task Microservices', () => {
+  describe('POST /createTask', () => {
+    it('should return a 201 response with the taskid and message', async () => {
       const response = await request(app)
-        .post("/createTask")
-        .set("Accept", "application/json")
-        .send({ "username": "SERtestuser",
-                "password": "testuser",
-                "projectname":"testProject",
-                "userstoryname":"test US3",
-                "taskname":"TASK1-TESTCASE" });
+        .post('/createTask')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US3',
+          taskname: 'TASK1-TESTCASE',
+        });
       expect(response.status).toBe(201);
       expect(response.body.taskId).toBeDefined();
     });
-    it("should return a 500 response with an error message if task is not created", async () => {
+    it('should return a 500 response with an error message if task is not created', async () => {
       const response = await request(app)
-        .post("/createTask")
-        .set("Accept", "application/json")
-        .send({ "username": "SERtestuser",
-                "password": "testuser",
-                "projectname":"testProject",
-                "userstoryname":"test US",
-                "taskname":"TASK1-TESTCASE" });
+        .post('/createTask')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US',
+          taskname: 'TASK1-TESTCASE',
+        });
       expect(response.status).toBe(500);
     });
   });
-
+  describe('POST /updateTask', () => {
+    it('should return a 201 response with the taskid and updation message', async () => {
+      const response = await request(app)
+        .post('/updateTask')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US3',
+          taskname: 'TASK1-TESTCASE',
+          description: 'Testing personal data',
+          status: 'In Progress',
+        });
+      expect(response.status).toBe(201);
+      expect(response.body.taskId).toBeDefined();
+    });
+    it('should return a 500 response with an error message if task is not updated', async () => {
+      const response = await request(app)
+        .post('/updateTask')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectname: 'testProject',
+          userstoryname: 'test US3',
+          taskname: 'TASK1-Customer',
+          description: 'Testing personal data',
+          status: 'In Progress',
+        });
+      expect(response.status).toBe(500);
+    });
+  });
 });
