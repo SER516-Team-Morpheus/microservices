c8335cdd684f2bea64007d5c6f4663710b8cd568
sambhavv14asu <skapoo26@asu.edu>
2023-04-08 02:26:59 -0700

    Adding test cases for updating role of members

--- src/memberservice/test.js
+++ src/memberservice/test.js
@@ -1,6 +1,5 @@
 const request = require('supertest')
 const app = require('./index')
-const axios = require('axios')
 
 describe('Member Microservice', () => {
   describe('POST /createMember', () => {
@@ -55,6 +54,34 @@ describe('Member Microservice', () => {
           username: 'sambhavv14asu',
           password: 'Wisdommarrt_01',
           projectId: 720
+        })
+      expect(response.status).toBe(500)
+    })
+  })
+
+  describe('PATCH /editMemberRole', () => {
+    it('should return a 201 response ', async () => {
+      const response = await request(app)
+        .patch('/editMemberRole')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          roleId: 4371742,
+          memberId: 1446686
+        })
+      expect(response.status).toBe(201)
+      expect(response.body.data).toBeDefined()
+    })
+    it('should return a 500 response ', async () => {
+      const response = await request(app)
+        .patch('/editMemberRole')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'sambhavv14asu',
+          password: 'Wisdommarrt_01',
+          roleId: 4371742,
+          memberId: 144
         })
       expect(response.status).toBe(500)
     })
