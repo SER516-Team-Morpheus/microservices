34987ec355cc0c4db80880b124e1d72546916be2
Chenxi6132 <chenxix2@asu.edu>
2023-04-09 16:06:21 -0700

    fix sytax erro for logic file

--- src/addroleservice/logic.js
+++ src/addroleservice/logic.js
@@ -1,123 +1,120 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
-// This will have the logic of the endpoints.
-//const fetch = require("node-fetch");
-const axios = require("axios");
-//const dotenv = import("dotenv");
+const dotenv = require('dotenv')
+dotenv.config({ path: '../.env' })
+const axios = require('axios')
+require('dotenv').config({ path: '../.env' })
 
-require('dotenv').config({ path: '../.env' });
-
-const ROLE_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles`;
-const AUTH_URL = `${process.env.AUTHENTICATE_URL}`;
+const ROLE_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles`
+const AUTH_URL = `${process.env.AUTHENTICATE_URL}`
 
 // Function to get auth token from authenticate api
-async function getToken(username, password) {
+async function getToken (username, password) {
   try {
     const response = await axios.post(AUTH_URL, {
       type: 'normal',
       username,
-      password,
-    });
+      password
+    })
     if (response.data.token) {
-      return response.data.token;
+      return response.data.token
     }
-    return { auth_token: 'NULL' };
+    return { auth_token: 'NULL' }
   } catch (error) {
-    return { auth_token: 'NULL' };
+    return { auth_token: 'NULL' }
   }
 }
 
 // create new roles
-async function createRoles(name, project, order, computable, permissions, token) {
+async function createRoles (name, project, order, computable, permissions, token) {
   const data = {
     name,
     project,
     order,
     computable,
-    permissions,
-  };
+    permissions
+  }
 
   const response = await fetch(ROLE_API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`
     },
-    body: JSON.stringify(data),
-  });
+    body: JSON.stringify(data)
+  })
 
   if (!response.ok) {
-    const errorMessage = `Failed to create role: ${response.statusText}`;
-    throw new Error(errorMessage);
+    const errorMessage = `Failed to create role: ${response.statusText}`
+    throw new Error(errorMessage)
   }
 
-  const role = await response.json();
+  const role = await response.json()
 
-  return role;
+  return role
 }
 
 // update roles
-async function updateRole(roleId, name, order, computable, permissions, token) {
-  const ROLE_UPDATE_API_URL = ROLE_API_URL + "/" + roleId;
-  const data = {};
+async function updateRole (roleId, name, order, computable, permissions, token) {
+  const ROLE_UPDATE_API_URL = ROLE_API_URL + '/' + roleId
+  const data = {}
 
   if (name) {
-    data.name = name;
+    data.name = name
   }
 
   if (order !== undefined) {
-    data.order = order;
+    data.order = order
   }
 
   if (computable !== undefined) {
-    data.computable = computable;
+    data.computable = computable
   }
 
   if (permissions) {
-    data.permissions = permissions;
+    data.permissions = permissions
   }
 
   const response = await fetch(ROLE_UPDATE_API_URL, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`
     },
-    body: JSON.stringify(data),
-  });
+    body: JSON.stringify(data)
+  })
 
   if (!response.ok) {
-    const errorMessage = `Failed to update role: ${response.statusText}`;
-    throw new Error(errorMessage);
+    const errorMessage = `Failed to update role: ${response.statusText}`
+    throw new Error(errorMessage)
   }
 
-  const role = await response.json();
+  const role = await response.json()
 
-  return role;
+  return role
 }
 
 // get roles details
-async function getRoleDetails(roleId, token) {
-  const ROLE_DETAILS_API_URL = ROLE_API_URL + "/" + roleId;
+async function getRoleDetails (roleId, token) {
+  const ROLE_DETAILS_API_URL = ROLE_API_URL + '/' + roleId
   const response = await fetch(ROLE_DETAILS_API_URL, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
+      Authorization: `Bearer ${token}`
+    }
+  })
 
   if (!response.ok) {
-    const errorMessage = `Failed to get role: ${response.statusText}`;
-    throw new Error(errorMessage);
+    const errorMessage = `Failed to get role: ${response.statusText}`
+    throw new Error(errorMessage)
   }
 
-  const role = await response.json();
+  const role = await response.json()
 
-  return role;
+  return role
 }
 
 module.exports = {
+  getToken,
   createRoles,
   updateRole,
-  getRoleDetails,
-};
+  getRoleDetails
+}
