2b4be24be93b1bc9e63a80b2ec9ffba0061bf48a
parvd <parvd5@gmail.com>
2023-04-08 18:03:51 -0700

    Added ESlint to project service

--- src/projectservice/index.js
+++ src/projectservice/index.js
@@ -1,97 +1,110 @@
-const express = require("express");
-const { getToken, getProjectBySlug, createProject, editProject, deleteProject } = require("./logic");
+const express = require('express')
+const { getToken, getProjectBySlug, createProject, editProject, deleteProject } = require('./logic')
 
-const app = express();
-const port = 3002;
+const app = express()
+const port = 3002
 
-app.use(express.json());
+app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods", "GET, POST, PUT, DELETE')
+  next()
+})
 
-//Endpoint for getting project by slug name
-app.get("/getProjectBySlug", async (req, res) => {
-  const { username, password, name } = req.query;
-  const token = await getToken(username, password);
-  const slugName = `${username.toLowerCase()}-${name.toLowerCase()}`;
-  const projectData = await getProjectBySlug(token, slugName);
+// Endpoint for getting project by slug name
+app.get('/getProjectBySlug', async (req, res) => {
+  const { username, password, name } = req.query
+  const token = await getToken(username, password)
+  const slugName = `${username.toLowerCase()}-${name.toLowerCase()}`
+  const projectData = await getProjectBySlug(token, slugName)
   if (!projectData.success) {
-    return res.status(404).send(projectData);
+    return res.status(404).send(projectData)
   }
-  return res.send(projectData);
-});
+  return res.send(projectData)
+})
 
 // Endpoint for creating a new project
-app.post("/createProject", async (req, res) => {
-  const { username, password, name, description } = req.body;
-  const token = await getToken(username, password);
-  const slugName = username.toLowerCase() + "-" + name.toLowerCase();
-  const checkProjectName = await getProjectBySlug(token, slugName);
+app.post('/createProject', async (req, res) => {
+  const { username, password, name, description } = req.body
+  const token = await getToken(username, password)
+  const slugName = username.toLowerCase() + '-' + name.toLowerCase()
+  const checkProjectName = await getProjectBySlug(token, slugName)
   if (!checkProjectName.success) {
-    const projectData = await createProject(name, description, token);
+    const projectData = await createProject(name, description, token)
     if (!projectData.success) {
-      return res.status(500).send(projectData);
+      return res.status(500).send(projectData)
     }
-    return res.status(201).send(projectData);
+    return res.status(201).send(projectData)
   }
   return res.status(500).send({
     success: false,
-    message: "Same project name already exist!",
-    sol: "Try different project name.",
-  });
-});
+    message: 'Same project name already exist!',
+    sol: 'Try different project name.'
+  })
+})
 
 // Endpoint for deleting a project
 app.delete('/deleteProject/:projectID', async (req, res) => {
-  let token = req.body.token;
+  let token = req.body.token
   if (!token) {
-    const { username, password } = req.body;
-    token = await getToken(username, password);
+    const { username, password } = req.body
+    token = await getToken(username, password)
   }
   const projectID = req.params.projectID
-  console.log(projectID,token)
   try {
-    const status = await deleteProject( projectID,token);
-    const ack = {
-      "projectID": projectID,
-      "status": "Deleted Successfully",
-      "TaigaAPIResponseStatus": status
+    const status = await deleteProject(projectID, token)
+    const data = {
+      // eslint-disable-next-line quote-props
+      'status': status.message,
+      // eslint-disable-next-line quote-props
+      'TaigaAPIResponseStatus': status.success
     }
-    res.status(201).send(ack);
+    if (status.success) {
+      res.status(201).send(data)
+    } else {
+      res.status(500).send({
+        message: 'Project ID is invalid'
+      })
+    }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
-});
+})
 
-//Endpoint for Updating a Project
+// Endpoint for Updating a Project
 app.patch('/updateProject/:projectID', async (req, res) => {
-  let token = req.body.token;
+  let token = req.body.token
   if (!token) {
-    const { username, password } = req.body;
-    token = await getToken(username, password);
+    const { username, password } = req.body
+    token = await getToken(username, password)
   }
-  const projectID = req.params.projectID;
-  const patch = req.body.patch;
+  const projectID = req.params.projectID
+  const patch = req.body.patch
   try {
-    const editedSprint = await editProject(token, projectID, patch);
-    res.status(201).json(editedSprint);
+    const projectData = await editProject(token, projectID, patch)
+    // eslint-disable-next-line eqeqeq
+    if (projectData.success == true) {
+      res.status(201).json(projectData.data)
+    } else {
+      res.status(500).json({
+        message: 'Error in editing project name'
+      })
+    }
   } catch (error) {
-    res.status(500).json({ error: 'Error editing project' });
+    res.status(500).json({ error: 'Error editing project' })
   }
-});
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(
     `Create Project microservice running at http://localhost:${port}`
-  );
-});
+  )
+})
 
-module.exports = app;
+module.exports = app
--- src/projectservice/logic.js
+++ src/projectservice/logic.js
@@ -1,65 +1,65 @@
-const axios = require("axios");
-require("dotenv").config({ path: "../.env" });
+const axios = require('axios')
+require('dotenv').config({ path: '../.env' })
 
-const PROJECT_API_URL = `${process.env.TAIGA_API_BASE_URL}/projects`;
-const AUTH_URL = `${process.env.AUTHENTICATE_URL}`;
+const PROJECT_API_URL = `${process.env.TAIGA_API_BASE_URL}/projects`
+const AUTH_URL = `${process.env.AUTHENTICATE_URL}`
 
-//Function to get auth token from authenticate api
-async function getToken(username, password) {
+// Function to get auth token from authenticate api
+async function getToken (username, password) {
   try {
     const response = await axios.post(AUTH_URL, {
-      type: "normal",
+      type: 'normal',
       username,
-      password,
-    });
+      password
+    })
     if (response.data.auth_token) {
-      return response.data.auth_token;
+      return response.data.auth_token
     } else {
-      return { auth_token: "NULL" };
+      return { auth_token: 'NULL' }
     }
   } catch (error) {
-    return { auth_token: "NULL" };
+    return { auth_token: 'NULL' }
   }
 }
 
 // Function to get the projects by slug name
-async function getProjectBySlug(token, slugName) {
-  PROJECT_SLUG_URL = PROJECT_API_URL + "/by_slug?slug=" + slugName;
+async function getProjectBySlug (token, slugName) {
+  const PROJECT_SLUG_URL = PROJECT_API_URL + '/by_slug?slug=' + slugName
   try {
     const response = await axios.get(PROJECT_SLUG_URL, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+      headers: { Authorization: `Bearer ${token}` }
+    })
     if (response.data.id) {
       return {
         success: true,
         projectId: response.data.id,
         projectName: response.data.name,
         slugName: response.data.slug,
-        description: response.data.description,
-      };
+        description: response.data.description
+      }
     } else {
       return {
         success: false,
-        message: "No project found",
-      };
+        message: 'No project found'
+      }
     }
   } catch (error) {
-    return { success: false, message: "Error getting project by name" };
+    return { success: false, message: 'Error getting project by name' }
   }
 }
 
 // Function to create new project
-async function createProject(name, description, token) {
+async function createProject (name, description, token) {
   try {
     const response = await axios.post(
       PROJECT_API_URL,
       {
         name,
         description,
-        is_private: false,
+        is_private: false
       },
       { headers: { Authorization: `Bearer ${token}` } }
-    );
+    )
 
     if (response.data.id) {
       return {
@@ -67,55 +67,64 @@ async function createProject(name, description, token) {
         projectId: response.data.id,
         projectName: response.data.name,
         slugName: response.data.slug,
-        description: response.data.description,
-      };
+        description: response.data.description
+      }
     } else {
       return {
         success: false,
-        message: "Something went wrong will creating project",
-      };
+        message: 'Something went wrong will creating project'
+      }
     }
   } catch (error) {
-    return { success: false, message: "Error creating project" };
+    return { success: false, message: 'Error creating project' }
   }
 }
 
 // Function to delete project
-async function deleteProject(projectID, token) {
+async function deleteProject (projectID, token) {
   try {
-    const DELETE_URL = PROJECT_API_URL + "/" + projectID
+    const DELETE_URL = PROJECT_API_URL + '/' + projectID
     const response = await axios.delete(
       DELETE_URL,
       { headers: { Authorization: `Bearer ${token}` } }
-    );
+    )
 
+    // eslint-disable-next-line eqeqeq
     if (response.status == 204) {
       return {
         success: true,
-        message: "Project Deleted Successfully"
-      };
+        message: 'Project Deleted Successfully'
+      }
     } else {
       return {
         success: false,
-        message: "Something went wrong will deleting project",
-      };
+        message: 'Something went wrong will deleting project'
+      }
     }
   } catch (error) {
-    return { success: false, message: "Error deleting project" };
+    return { success: false, message: 'Error deleting project' }
   }
 }
 
 //
-async function editProject(token, projectID, patch) {
+async function editProject (token, projectID, patch) {
   try {
     const headers = {
-      "Content-Type": "application/json",
-      'Authorization': `Bearer ${token}`
-    };
-    const response = await axios.patch(`${PROJECT_API_URL}/${projectID}`, patch, { headers });
-    return response.data;
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+    const response = await axios.patch(`${PROJECT_API_URL}/${projectID}`, patch, { headers })
+    // eslint-disable-next-line eqeqeq
+    if (response.status == 200) {
+      return {
+        status: 'success',
+        data: response.data
+      }
+    }
+    console.log(response.status)
+    return response.data
   } catch (error) {
-    throw new Error('Error editing project');
+    throw new Error('Error editing project')
   }
 }
 module.exports = {
@@ -124,4 +133,4 @@ module.exports = {
   getToken,
   deleteProject,
   editProject
-};
+}
