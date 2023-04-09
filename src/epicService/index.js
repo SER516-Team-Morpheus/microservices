9c3adf9d0595ef0a5d2fde64a2773723205016ac
VidhinASU <vidhin@asu.edu>
2023-04-09 11:58:58 -0700

    Lint packages

--- src/epicService/index.js
+++ src/epicService/index.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const { getToken, createEpic } = require("./logic");
+const express = require('express')
+const { getToken, createEpic } = require('./logic')
 
-const app = express();
-const port = 3006;
+const app = express()
+const port = 3006
 
-app.use(express.json());
+app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
+})
 
 // Endpoint for creating a new epic
-app.post("/createEpic", async (req, res) => {
-  const { username, password, project_id, name, description } = req.body;
-  const token = await getToken(username, password);
-  const epicData = await createEpic(name, project_id, description, token);
+app.post('/createEpic', async (req, res) => {
+  const { username, password, projectId, name, description } = req.body
+  const token = await getToken(username, password)
+  const epicData = await createEpic(name, projectId, description, token)
   if (!epicData.success) {
-    return res.status(500).send(epicData);
+    return res.status(500).send(epicData)
   }
-  return res.status(201).send(epicData);
-});
+  return res.status(201).send(epicData)
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(
     `Create Epic microservice running at http://localhost:${port}`
-  );
-});
+  )
+})
 
-module.exports = app;
+module.exports = app
--- src/epicService/logic.js
+++ src/epicService/logic.js
@@ -1,29 +1,29 @@
-const axios = require("axios");
-require("dotenv").config({ path: "../.env" });
+const axios = require('axios')
+require('dotenv').config({ path: '../.env' })
 
-const EPIC_API_URL = `${process.env.TAIGA_API_BASE_URL}/epics`;
-const AUTH_URL = `${process.env.AUTHENTICATE_URL}`;
+const EPIC_API_URL = `${process.env.TAIGA_API_BASE_URL}/epics`
+const AUTH_URL = `${process.env.AUTHENTICATE_URL}`
 
 // Function to get auth token from authenticate api
-async function getToken(username, password) {
+async function getToken (username, password) {
   try {
     const response = await axios.post(AUTH_URL, {
-      type: "normal",
+      type: 'normal',
       username,
-      password,
-    });
+      password
+    })
     if (response.data.token) {
-      return response.data.token;
+      return response.data.token
     } else {
-      return { auth_token: "NULL" };
+      return { auth_token: 'NULL' }
     }
   } catch (error) {
-    return { auth_token: "NULL" };
+    return { auth_token: 'NULL' }
   }
 }
 
 // Function to create new epic
-async function createEpic(
+async function createEpic (
   name,
   project,
   description,
@@ -37,31 +37,31 @@ async function createEpic(
       {
         subject: name,
         project,
-        description,
+        description
       },
       { headers: { Authorization: `Bearer ${token}` } }
-    );
-    console.log(response.data);
+    )
+    console.log(response.data)
     if (response.data.id) {
       return {
         success: true,
         epicId: response.data.id,
         epicName: response.data.subject,
-        description: response.data.description,
-      };
+        description: response.data.description
+      }
     } else {
       return {
         success: false,
-        message: "Something went wrong while creating epic",
-      };
+        message: 'Something went wrong while creating epic'
+      }
     }
   } catch (error) {
-    console.log(error);
-    return { success: false, message: "Error creating epic" };
+    console.log(error)
+    return { success: false, message: 'Error creating epic' }
   }
 }
 
 module.exports = {
   createEpic,
-  getToken,
-};
+  getToken
+}
