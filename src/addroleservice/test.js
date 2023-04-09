d84bee1c72951769f72b2869c86f4d2b8da4c6c4
naheer <nnolas33@asu.edu>
2023-04-09 15:57:47 -0700

    fixed syntax errors

--- src/addroleservice/test.js
+++ src/addroleservice/test.js
@@ -1,55 +1,53 @@
-const { createRoles } = require('./logic');
-
-
+const { createRoles } = require('./logic')
 describe('createRoles', () => {
   test('should create a new role', async () => {
-    const token = 'mock_token';
-    const name = 'test_role';
-    const project = 1;
-    const order = 2;
-    const computable = true;
-    const permissions = [1, 2];
+    const token = 'mock_token'
+    const name = 'test_role'
+    const project = 1
+    const order = 2
+    const computable = true
+    const permissions = [1, 2]
 
     // Mock the fetch API call
     global.fetch = jest.fn().mockImplementation(() =>
       Promise.resolve({
         ok: true,
-        json: () => Promise.resolve({ id: 1, name, project, order, computable, permissions }),
+        json: () => Promise.resolve({ id: 1, name, project, order, computable, permissions })
       })
-    );
+    )
 
-    const role = await createRoles(name, project, order, computable, permissions, token);
+    const role = await createRoles(name, project, order, computable, permissions, token)
 
     expect(fetch).toHaveBeenCalledWith('https://api.taiga.io/api/v1/roles', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`
       },
-      body: JSON.stringify({ name, project, order, computable, permissions }),
-    });
+      body: JSON.stringify({ name, project, order, computable, permissions })
+    })
 
-    expect(role).toEqual({ id: 1, name, project, order, computable, permissions });
-  });
+    expect(role).toEqual({ id: 1, name, project, order, computable, permissions })
+  })
 
   test('should throw an error if the API call fails', async () => {
-    const token = 'mock_token';
-    const name = 'test_role';
-    const project = 1;
-    const order = 2;
-    const computable = true;
-    const permissions = [1, 2];
+    const token = 'mock_token'
+    const name = 'test_role'
+    const project = 1
+    const order = 2
+    const computable = true
+    const permissions = [1, 2]
 
     // Mock the fetch API call to return an error
     global.fetch = jest.fn().mockImplementation(() =>
       Promise.resolve({
         ok: false,
-        statusText: 'Internal Server Error',
+        statusText: 'Internal Server Error'
       })
-    );
+    )
 
     await expect(createRoles(name, project, order, computable, permissions, token)).rejects.toThrow(
       'Failed to create role: Internal Server Error'
-    );
-  });
-});
+    )
+  })
+})
