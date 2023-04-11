28b27b581b96803cf03e6e0bc54a91984f5339aa
Subham Kumar (Student) <skuma251@sundevils.asu.edu>
2023-04-11 00:37:40 -0700

    Added a new endpoint to get all the roles in a project

--- src/rolesservice/index.js
+++ src/rolesservice/index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const logic = require('./logic')
+const { getToken, getAllRoles } = require('./logic')
 
 const app = express()
 app.use(bodyParser.json())
@@ -22,12 +23,16 @@ app.use((req, res, next) => {
 
 // Endpoint for creating a role
 app.post('/createroles', async (req, res, next) => {
-  const {
-    name, project, order, computable, permissions
-  } = req.body
+  const { name, project, order, computable, permissions } = req.body
 
   try {
-    const role = await logic.createRoles(name, project, order, computable, permissions)
+    const role = await logic.createRoles(
+      name,
+      project,
+      order,
+      computable,
+      permissions
+    )
     res.status(201).json(role)
   } catch (error) {
     next(error)
@@ -37,12 +42,16 @@ app.post('/createroles', async (req, res, next) => {
 // Endpoint for updating a role
 app.patch('/updateroles/:roleId', async (req, res, next) => {
   const { roleId } = req.params
-  const {
-    name, order, computable, permissions
-  } = req.body
+  const { name, order, computable, permissions } = req.body
 
   try {
-    const role = await logic.updateRole(roleId, name, order, computable, permissions)
+    const role = await logic.updateRole(
+      roleId,
+      name,
+      order,
+      computable,
+      permissions
+    )
     res.status(201).json(role)
   } catch (error) {
     next(error)
@@ -50,19 +59,21 @@ app.patch('/updateroles/:roleId', async (req, res, next) => {
 })
 
 // Endpoint for geting a role
-app.get('/getroles/:roleId', async (req, res, next) => {
-  const { roleId } = req.params
-
-  try {
-    const role = await logic.getRoleDetails(roleId)
-    res.status(201).json(role)
-  } catch (error) {
-    next(error)
+app.get('/getroles', async (req, res, next) => {
+  const { username, password, projectName } = req.query
+  const token = await getToken(username, password)
+  const slugName = `${username.toLowerCase()}-${projectName.toLowerCase()}`
+  const projectData = await getAllRoles(token, slugName)
+  if (!projectData.success) {
+    return res.status(404).send(projectData)
   }
+  return res.send(projectData)
 })
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Create Member addroleservice running at http://localhost:${port}`)
+  console.log(
+    `Create Member addroleservice running at http://localhost:${port}`
+  )
 })
 module.exports = app
--- src/rolesservice/logic.js
+++ src/rolesservice/logic.js
@@ -4,18 +4,19 @@ const axios = require('axios')
 require('dotenv').config({ path: '../.env' })
 
 const ROLE_API_URL = `${process.env.TAIGA_API_BASE_URL}/roles`
-const AUTH_URL = `${process.env.AUTHENTICATE_URL}`
+const AUTH_URL = `${process.env.TAIGA_API_BASE_URL}/auth`
+const PROJECT_URL = `${process.env.TAIGA_API_BASE_URL}/projects`
 
 // Function to get auth token from authenticate api
-async function getToken (username, password) {
+async function getToken(username, password) {
   try {
     const response = await axios.post(AUTH_URL, {
       type: 'normal',
       username,
-      password
+      password,
     })
-    if (response.data.token) {
-      return response.data.token
+    if (response.data.auth_token) {
+      return response.data.auth_token
     }
     return { auth_token: 'NULL' }
   } catch (error) {
@@ -23,23 +24,60 @@ async function getToken (username, password) {
   }
 }
 
+// Function to get the roles by slug name
+async function getAllRoles(token, slugName) {
+  const ROLE_SLUG_URL = PROJECT_URL + '/by_slug?slug=' + slugName
+  try {
+    const response = await axios.get(ROLE_SLUG_URL, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    const rolesArray = response.data.roles
+    const newResponse = []
+    for (let i = 0; i < rolesArray.length; i++) {
+      const roleId = rolesArray[i].id
+      const roleName = rolesArray[i].name
+      newResponse.push({ roleId, roleName })
+    }
+    if (newResponse.length) {
+      return {
+        success: true,
+        userStory: newResponse,
+      }
+    } else {
+      return {
+        success: false,
+        message: 'No roles found.',
+      }
+    }
+  } catch (error) {
+    return { success: false, message: 'Error getting roles' }
+  }
+}
+
 // create new roles
-async function createRoles (name, project, order, computable, permissions, token) {
+async function createRoles(
+  name,
+  project,
+  order,
+  computable,
+  permissions,
+  token
+) {
   const data = {
     name,
     project,
     order,
     computable,
-    permissions
+    permissions,
   }
 
   const response = await fetch(ROLE_API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
 
   if (!response.ok) {
@@ -53,7 +91,7 @@ async function createRoles (name, project, order, computable, permissions, token
 }
 
 // update roles
-async function updateRole (roleId, name, order, computable, permissions, token) {
+async function updateRole(roleId, name, order, computable, permissions, token) {
   const ROLE_UPDATE_API_URL = ROLE_API_URL + '/' + roleId
   const data = {}
 
@@ -77,9 +115,9 @@ async function updateRole (roleId, name, order, computable, permissions, token) 
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
 
   if (!response.ok) {
@@ -93,13 +131,13 @@ async function updateRole (roleId, name, order, computable, permissions, token) 
 }
 
 // get roles details
-async function getRoleDetails (roleId, token) {
+async function getRoleDetails(roleId, token) {
   const ROLE_DETAILS_API_URL = ROLE_API_URL + '/' + roleId
   const response = await fetch(ROLE_DETAILS_API_URL, {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   })
 
   if (!response.ok) {
@@ -116,5 +154,6 @@ module.exports = {
   getToken,
   createRoles,
   updateRole,
-  getRoleDetails
+  getRoleDetails,
+  getAllRoles,
 }
