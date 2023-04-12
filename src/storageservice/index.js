3dfac9ccf14eedf643268c77266801dace839c85
Aakanksha Jain <112651729+ajain298@users.noreply.github.com>
2023-04-11 23:45:51 -0700

    Update index.js

--- src/storageservice/index.js
+++ src/storageservice/index.js
@@ -1,1 +1,21 @@
+const express = require('express')
+const { getCFDData } = require('./logic')
 
+const app = express()
+const port = 3010
+
+app.get('/cfd-data', async (req, res) => {
+  try {
+    const cfdData = await getCFDData()
+    res.status(200).json(cfdData)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+app.listen(port, () => {
+  console.log(`CFD microservice running at http://localhost:${port}`)
+})
+
+module.exports = app
