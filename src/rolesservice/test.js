3bb33be13a9009f8a6b4f696c024cf4a1e2ad101
skuma251 <skuma251@asu.edu>
2023-04-12 00:37:59 -0700

    Wrote test case for delete role endpoint

--- src/rolesservice/test.js
+++ src/rolesservice/test.js
@@ -4,10 +4,9 @@ const app = require('./index')
 
 describe('Role Microservice', () => {
   describe('GET /getroles', () => {
-    it('should return a 200 response', async () => {
+    it('should return a 200 response with roles', async () => {
       const response = await request(app)
         .get('/getroles')
-        .set('Accept', 'application/json')
         .query({
           username: 'SERtestuser',
           password: 'testuser',
@@ -17,19 +16,18 @@ describe('Role Microservice', () => {
       expect(response.body.success).toBe(true)
       expect(response.body.roles).toBeDefined()
     })
-    // it('should return a 404 response', async () => {
-    //   const response = await request(app)
-    //     .get('/getroles')
-    //     .set('Accept', 'application/json')
-    //     .query({
-    //       username: 'SERtestuser',
-    //       password: 'tetuser',
-    //       name: 'testProject1',
-    //     })
-    //   expect(response.status).toBe(404)
-    //   expect(response.body.success).toBe(false)
-    //   expect(response.body.message).toBeDefined()
-    // }, 30000)
+    it('should return a 404 response with a error message', async () => {
+      const response = await request(app)
+        .get('/getroles')
+        .query({
+          username: 'SERtestuser',
+          password: 'tetuser',
+          projectName: 'noProject',
+        })
+      expect(response.status).toBe(404)
+      expect(response.body.success).toBe(false)
+      expect(response.body.message).toBeDefined()
+    })
   })
   describe('POST /createroles', () => {
     it('should return a 201 response', async () => {
@@ -46,18 +44,18 @@ describe('Role Microservice', () => {
       expect(response.body.success).toBe(true)
       expect(response.body.roleName).toBeDefined()
     })
-    // it('should return a 404 response', async () => {
-    //   const response = await request(app)
-    //     .post('/createroles')
-    //     .set('Accept', 'application/json')
-    //     .send({
-    //       username: 'SERtestuser',
-    //       password: 'tetuser',
-    //       roleName: 'testDevRole',
-    //       name: 'testProject'
-    //     })
-    //   expect(response.status).toBe(404)
-    // }, 20000)
+    it('should return a 404 response', async () => {
+      const response = await request(app)
+        .post('/createroles')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'SERtestuser',
+          password: 'tetuser',
+          roleName: 'testDevRole',
+          projectName: 'testProject'
+        })
+      expect(response.status).toBe(404)
+    }, 20000)
   })
 
   describe('PATCH /updateroles', () => {
@@ -91,5 +89,29 @@ describe('Role Microservice', () => {
       expect(response.body.success).toBe(false)
     })
   })
-  
+
+  describe('DELETE /deleteroles/:roleName', () => {
+    it('should return a 200 response and delete the role', async () => {
+      const response = await request(app)
+        .delete('/deleteroles/testDevRole')
+        .query({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'testProject'
+        })
+      expect(response.status).toBe(200)
+      expect(response.body.success).toBe(true)
+    })
+    it('should return a 404 response if no roles are found', async () => {
+      const response = await request(app)
+        .delete('/deleteroles/notestDevRole')
+        .set('Accept', 'application/json')
+        .query({
+          username: 'SERtestuser',
+          password: 'testuser',
+          projectName: 'testProject'
+        })
+      expect(response.status).toBe(404)
+    })
+  })
 })
