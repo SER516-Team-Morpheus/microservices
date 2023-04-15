f292152c5928fcd529c614ab91ed6cbed826f035
Raumil D <rdhandhu@asu.edu>
2023-04-15 13:36:51 -0700

    Unit testing of CFD Service

--- src/cfdservice/test.js
+++ src/cfdservice/test.js
@@ -1,3 +1,43 @@
 
 const request = require('supertest')
-const app = require('./index')
+const { app, getEmptyStatusMatrix } = require('./index');
+
+describe('GET /cfd', () => {
+    test('returns CFD data for the given project name', async () => {
+        const projectName = 'TestProject';
+        const response = await request(app)
+            .get('/cfd')
+            .send({ projectName: projectName })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(response.body.project).toEqual(projectName);
+    }, 15000);
+
+    test('returns an error if project not found', async () => {
+        const projectName = 'NonexistentProject';
+        const response = await request(app)
+            .get('/cfd')
+            .send({ projectName: projectName })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(500);
+        expect(response.body.error).toEqual('Project Not Found');
+    }, 15000);
+});
+
+describe('getEmptyStatusMatrix', () => {
+    test('returns the task statuses for a valid project', async () => {
+        const slug = 'sertestuser-testproject';
+        const result = await getEmptyStatusMatrix(slug);
+        expect(result.success).toBe(true);
+        expect(result.statuses).toBeDefined();
+    }, 15000);
+
+    test('returns an error for an invalid project', async () => {
+        const slug = 'invalid-project';
+        const result = await getEmptyStatusMatrix(slug);
+        expect(result.success).toBe(false);
+        expect(result.error).toEqual('Error Finding Project');
+    }, 15000);
+});
