3cd11df90f39da952dc7df747912e11ff82a0ee9
Raumil D <rdhandhu@asu.edu>
2023-04-15 14:06:56 -0700

    Formatted code according to Lint requirements

--- src/cfdservice/index.js
+++ src/cfdservice/index.js
@@ -1,119 +1,119 @@
+/* eslint-disable no-undef */
 const express = require('express')
 const AWS = require('aws-sdk')
 const {
-    fetchData, getHeaders, getToken, getTaskStatuses, getProjectID
+  getHeaders, getToken, getTaskStatuses, getProjectID
 } = require('./logic')
-const dynamodb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" })
+const dynamodb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 const app = express()
 const port = 3012
 
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    next()
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  next()
 })
 
-function queryDynamoDB(params) {
-    return new Promise((resolve, reject) => {
-        dynamodb.query(params, function (err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data.Items);
-            }
-        });
-    });
+function queryDynamoDB (params) {
+  return new Promise((resolve, reject) => {
+    dynamodb.query(params, function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data.Items)
+      }
+    })
+  })
 }
 
-async function getEmptyStatusMatrix(slug){
-    token = await getToken()
-    headers = getHeaders(token.token)
-    res = await getProjectID(headers, slug)
-    if (res.success){
-        return {statuses : await getTaskStatuses(headers, res.projectId), success: true}
-    } else {
-        return {error: "Error Finding Project", success: false}
-    }
+async function getEmptyStatusMatrix (slug) {
+  token = await getToken()
+  headers = getHeaders(token.token)
+  res = await getProjectID(headers, slug)
+  if (res.success) {
+    return { statuses: await getTaskStatuses(headers, res.projectId), success: true }
+  } else {
+    return { error: 'Error Finding Project', success: false }
+  }
 }
 
 // Endpoint for getting user stories details
 app.get('/cfd', async (req, res) => {
-    const userName = 'sertestuser';
-    const today = new Date();
-    const oneDay = 24 * 60 * 60 * 1000;
-    const endDate = new Date(today.getTime());
-    const sevenDays = 6 * oneDay;
-    const startDate = new Date(today.getTime() - sevenDays);
-    const { projectName} = req.body
-    const slug = `${userName.toLowerCase()}-${projectName.toLowerCase()}`
-    const dateList = [];
-    const cfdDataList = {};
-    const cfd = { project:projectName };
-    const statusData = await getEmptyStatusMatrix(slug)
-    if (statusData.success){
-        emptyStatusMatrix = statusData.statuses
+  const userName = 'sertestuser'
+  const today = new Date()
+  const oneDay = 24 * 60 * 60 * 1000
+  const endDate = new Date(today.getTime())
+  const sevenDays = 6 * oneDay
+  const startDate = new Date(today.getTime() - sevenDays)
+  const { projectName } = req.body
+  const slug = `${userName.toLowerCase()}-${projectName.toLowerCase()}`
+  const dateList = []
+  const cfdDataList = {}
+  const cfd = { project: projectName }
+  const statusData = await getEmptyStatusMatrix(slug)
+  if (statusData.success) {
+    emptyStatusMatrix = statusData.statuses
 
     // loop through each date between the start and end dates and add it to the list
+    // eslint-disable-next-line no-unmodified-loop-condition
     for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-        dateList.push(date.toISOString().slice(0, 10));
+      dateList.push(date.toISOString().slice(0, 10))
     }
 
     // create an array of Promises for each date in the dateList
     const promises = dateList.map(date => {
-        let key = date + ';' + slug
-        const params = {
-            TableName: 'ser516-cfd-data',
-            KeyConditionExpression: 'recordKey = :key',
-            ExpressionAttributeValues: {
-                ':key': key
-            }
+      const key = date + ';' + slug
+      const params = {
+        TableName: 'ser516-cfd-data',
+        KeyConditionExpression: 'recordKey = :key',
+        ExpressionAttributeValues: {
+          ':key': key
         }
-        return queryDynamoDB(params)
-            .then(data => {
-                cfdDataList[key] = data;
-                // code to execute after the data is fetched
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    });
+      }
+      return queryDynamoDB(params)
+        .then(data => {
+          cfdDataList[key] = data
+          // code to execute after the data is fetched
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    })
 
     // wait for all Promises to complete
     Promise.all(promises)
-        .then(() => {
-            // execute this code after all Promises are resolved
-            
-            Object.entries(cfdDataList).forEach(function ([ key, value]) {
-                const stringArray = key.split(';');
-                const date = stringArray[0]; 
-                if (Object.keys(value).length != 0) {
-                    delete value[0].recordKey
-                    cfd[date] = value[0]
-                } else {
-                    cfd[date] = emptyStatusMatrix
-                }
-            });
-            return res.status(200).send(cfd)
+      .then(() => {
+        // execute this code after all Promises are resolved
+
+        Object.entries(cfdDataList).forEach(function ([key, value]) {
+          const stringArray = key.split(';')
+          const date = stringArray[0]
+          // eslint-disable-next-line eqeqeq
+          if (Object.keys(value).length != 0) {
+            delete value[0].recordKey
+            cfd[date] = value[0]
+          } else {
+            cfd[date] = emptyStatusMatrix
+          }
         })
-        .catch(err => {
-            console.error(err);
-        });
-    } else {
-        return res.status(500).send({error: 'Project Not Found'})
-    }
+        return res.status(200).send(cfd)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  } else {
+    return res.status(500).send({ error: 'Project Not Found' })
+  }
 })
 // Start the server
 app.listen(port, () => {
-    console.log(`CFD Microservice running at http://localhost:${port}`)
+  console.log(`CFD Microservice running at http://localhost:${port}`)
 })
 
 module.exports = { app, getEmptyStatusMatrix }
-
-
-
--- src/cfdservice/logic.js
+++ src/cfdservice/logic.js
@@ -1,20 +1,15 @@
-const AWS = require('aws-sdk')
 const axios = require('axios')
-require('dotenv').config({ path: '../.env' })
-
-// Initialization DynamoDB client
-const dynamodb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 
 const TAIGA_BASE = 'https://api.taiga.io/api/v1'
 
-function getHeaders(token) {
+function getHeaders (token) {
   return {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`
   }
 }
 
-async function getToken() {
+async function getToken () {
   const body = {
     username: 'sertestuser',
     password: 'testuser',
@@ -26,13 +21,12 @@ async function getToken() {
     const res = await axios.post(url, body, { headers: { 'Content-Type': 'application/json' } })
     const data = res.data
     return { token: data.auth_token, success: true }
-  } catch(err) {
+  } catch (err) {
     return { error: err, success: false }
   }
-  
 }
 
-async function getProjectID(headers, slug) {
+async function getProjectID (headers, slug) {
   const PROJECT_SLUG_URL = TAIGA_BASE + '/projects/by_slug?slug=' + slug
   try {
     const response = await axios.get(PROJECT_SLUG_URL, {
@@ -54,7 +48,7 @@ async function getProjectID(headers, slug) {
   }
 }
 
-async function getTaskStatuses(headers, projectID) {
+async function getTaskStatuses (headers, projectID) {
   try {
     const statusUrl = `${TAIGA_BASE}/task-statuses?project=${projectID}`
     const statuses = await axios.get(statusUrl, { headers }).then((res) => res.data)
@@ -68,21 +62,4 @@ async function getTaskStatuses(headers, projectID) {
   }
 }
 
-// Function to fetch data from DynamoDB
-async function fetchData(params) {
-  try {
-    const data = dynamodb.query(params, (err, data) => {
-      if (err) {
-        return { success: false }
-      } else {
-        return { data: data.Items, success: true }
-      }
-    })
-    return { success: false }
-  } catch (error) {
-    console.log(`Error fetching data from DynamoDB: ${error}`);
-    return { success: false }
-  }
-}
-
-module.exports = { fetchData, getHeaders, getToken, getProjectID, getTaskStatuses };
+module.exports = { getHeaders, getToken, getProjectID, getTaskStatuses }
