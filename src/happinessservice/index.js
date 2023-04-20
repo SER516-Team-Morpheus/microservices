385c484dca6b779c384152356ac50b712286ac34
vchavhan <vchavhan@asu.edu>
2023-04-20 11:37:25 -0400

    Corrected the port and token logic as suggested

--- src/happinessservice/index.js
+++ src/happinessservice/index.js
@@ -11,9 +11,11 @@ app.use(express.json());
 
 // Endpoint for adding bv as a custom field
 app.post("/AddHappiness", async (req, res) => {
-  const { name, project } = req.body;
-  const token = req.headers.authorization.split(" ")[1];
-  const projectData = await AddBV(name, project, token);
+  const { name, project, token } = req.body;
+  if (!token){
+        token = req.headers.authorization.split(" ")[1];
+  }
+  const projectData = await AddHappiness(name, project, token);
   if (!projectData.success) {
     return res.status(500).send({
       success: false,
