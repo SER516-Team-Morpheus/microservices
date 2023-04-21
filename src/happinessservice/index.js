0c1d925b1f3362dcb3936cdefd5c507e9c0fe25e
vchavhan <vchavhan@asu.edu>
2023-04-21 13:12:16 -0400

    Corrected logic for index, taking username and password from request body

--- src/happinessservice/index.js
+++ src/happinessservice/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { AddBV } = require("./logic");
+const { AddHappiness } = require("./logic");
 
 const app = express();
 const port = 3030;
@@ -11,9 +11,10 @@ app.use(express.json());
 
 // Endpoint for adding bv as a custom field
 app.post("/AddHappiness", async (req, res) => {
-  const { name, project, token } = req.body;
-  if (!token){
-        token = req.headers.authorization.split(" ")[1];
+  const { username, password, projectId, name, description } = req.body
+  let { token } = req.body
+  if (!token) {
+    token = await getToken(username, password)
   }
   const projectData = await AddHappiness(name, project, token);
   if (!projectData.success) {
