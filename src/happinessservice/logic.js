5fba6544768c28f7979aafb0b4f7e06f2d27c17e
vchavhan <vchavhan@asu.edu>
2023-04-23 18:17:27 -0400

    Moved the grttoken function to logic

--- src/happinessservice/logic.js
+++ src/happinessservice/logic.js
@@ -2,7 +2,25 @@ const axios = require("axios");
 require("dotenv").config({ path: "../.env" });
 
 const PROJECT_API_URL = `${process.env.TAIGA_API_BASE_URL}/userstory-custom-attributes`;
+const AUTH_URL = `${process.env.TAIGA_API_BASE_URL}/auth`
 
+// Function to get auth token from authenticate api
+async function getToken (username, password) {
+  try {
+    const response = await axios.post(AUTH_URL, {
+      type: 'normal',
+      username,
+      password
+    })
+    if (response.data.auth_token) {
+      return response.data.auth_token
+    } else {
+      return { auth_token: 'NULL' }
+    }
+  } catch (error) {
+    return { auth_token: 'NULL' }
+  }
+}
 
 async function AddHappiness(name, project, token) {
   try {
